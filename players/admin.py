61dce75922c19996963dc910f50a81f9933d708b
James Kuny <james.kuny@yahoo.com>
2025-05-26 12:31:54 -0400

    Player model work

--- players/admin.py
+++ players/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
+from .models import Player, Organizer
+
+
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ('get_full_name', 'get_email', 'phone_number',
+                    'preferred_game')
+    search_fields = ('user__first_name', 'user__last_name', 'user__email')
+
+    def get_full_name(self, obj):
+        return f"{obj.user.first_name} {obj.user.last_name}"
+
+    def get_email(self, obj):
+        return obj.user.email
+
+    get_full_name.short_description = 'Full Name'
+    get_email.short_description = 'Email'
+
+    list_filter = ["preferred_game"]
+
+
 # Register your models here.
+admin.site.register(Player, PlayerAdmin)
+admin.site.register(Organizer, PlayerAdmin)
