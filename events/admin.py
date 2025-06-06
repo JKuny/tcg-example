6f1290dc7f08b101c497830943c7bd75d8f5b34a
James Kuny <James.Kuny@yahoo.com>
2025-06-06 14:52:01 -0400

    Events admin.py work

--- events/admin.py
+++ events/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 
-from events.models import Event
+from .models import Event
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('game_name', 'get_time_date')
+    search_fields = ('game_name', 'start_date')
+
+    def get_time_date(self, obj):
+        return f"{obj.start_date} {obj.start_time}"
+
+    get_time_date.short_description = 'Date'
+
+    list_filter = ["game_name"]
+
 
 # Register your models here.
-admin.site.register(Event)
+admin.site.register(Event, EventAdmin)
